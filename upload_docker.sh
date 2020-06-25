748a5702b78420edfca0731a41fc0ed9c963b405
Ravi K <ravindra.kande@gmail.com>
2020-06-25 16:37:33 -0400

    microservices

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+export dockerpath="ravi8636/ml-microservice"
 
 # Step 2:  
 # Authenticate & tag
+docker login --username ravi8636
+docker tag ml-microservice $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
